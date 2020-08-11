c3ab2dcafda7426a05349139e5d7a210751a8ceb
boyloe <boyloe@gmail.com>
2020-08-10 21:42:14 -0600

    Create transfer class

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status, :deposit
+    @@all = []
+    def initialize name, balance = 1000, status = 'open'
+        @name = name
+        @balance = balance
+        @status = status
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def deposit amount
+        self.balance += amount
+    end
+
+    def display_balance
+        p "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0 ? true : false
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
